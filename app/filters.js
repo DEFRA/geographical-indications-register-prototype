de64f6b0f8a1de6d628b96b12c0c4a0a185fd701
Martin Glancy <glancy@gmail.com>
2020-08-06 12:40:24 +0100

    Format date using a new filter

--- app/filters.js
+++ app/filters.js
@@ -38,6 +38,60 @@ module.exports = function (env) {
 
   ------------------------------------------------------------------ */
 
+  filters.formatShortDate = function(dateStr) {
+      dateStr = dateStr.replace("/01/", " Jan ")
+      dateStr = dateStr.replace("/02/", " Feb ")
+      dateStr = dateStr.replace("/03/", " Mar ")
+      dateStr = dateStr.replace("/04/", " Apr ")
+      dateStr = dateStr.replace("/05/", " May ")
+      dateStr = dateStr.replace("/06/", " Jun ")
+      dateStr = dateStr.replace("/07/", " Jul ")
+      dateStr = dateStr.replace("/08/", " Aug ")
+      dateStr = dateStr.replace("/09/", " Sep ")
+      dateStr = dateStr.replace("/10/", " Oct ")
+      dateStr = dateStr.replace("/11/", " Nov ")
+      dateStr = dateStr.replace("/12/", " Dec ")
+
+      dateStr = dateStr.replace("01 ", "1 ")
+      dateStr = dateStr.replace("02 ", "2 ")
+      dateStr = dateStr.replace("03 ", "3 ")
+      dateStr = dateStr.replace("04 ", "4 ")
+      dateStr = dateStr.replace("05 ", "5 ")
+      dateStr = dateStr.replace("06 ", "6 ")
+      dateStr = dateStr.replace("07 ", "7 ")
+      dateStr = dateStr.replace("08 ", "8 ")
+      dateStr = dateStr.replace("09 ", "9 ")
+
+      return dateStr
+  }
+
+  filters.formatLongDate = function(dateStr) {
+      dateStr = dateStr.replace("/01/", " January ")
+      dateStr = dateStr.replace("/02/", " February ")
+      dateStr = dateStr.replace("/03/", " March ")
+      dateStr = dateStr.replace("/04/", " April ")
+      dateStr = dateStr.replace("/05/", " May ")
+      dateStr = dateStr.replace("/06/", " June ")
+      dateStr = dateStr.replace("/07/", " July ")
+      dateStr = dateStr.replace("/08/", " August ")
+      dateStr = dateStr.replace("/09/", " September ")
+      dateStr = dateStr.replace("/10/", " October ")
+      dateStr = dateStr.replace("/11/", " November ")
+      dateStr = dateStr.replace("/12/", " December ")
+
+      dateStr = dateStr.replace("01 ", "1 ")
+      dateStr = dateStr.replace("02 ", "2 ")
+      dateStr = dateStr.replace("03 ", "3 ")
+      dateStr = dateStr.replace("04 ", "4 ")
+      dateStr = dateStr.replace("05 ", "5 ")
+      dateStr = dateStr.replace("06 ", "6 ")
+      dateStr = dateStr.replace("07 ", "7 ")
+      dateStr = dateStr.replace("08 ", "8 ")
+      dateStr = dateStr.replace("09 ", "9 ")
+
+      return dateStr
+  }
+
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
   ------------------------------------------------------------------ */
