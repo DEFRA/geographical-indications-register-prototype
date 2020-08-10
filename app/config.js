f51e3fedeb29049937e7d7017f8f91b8ff147817
Martin Glancy <glancy@gmail.com>
2020-08-10 08:03:49 +0100

    Change service name and 2 data columns

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Geographical indications register',
+  serviceName: 'Get information about protected foods, wines and spirits',
 
   // Default port that prototype runs on
   port: '3000',
