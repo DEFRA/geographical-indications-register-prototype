93b7e0cafd12407f507e81254d619e92def3dc33
Martin Glancy <glancy@gmail.com>
2020-08-29 09:37:43 +0100

    Add option to vary grid square size

--- app/assets/javascripts/gridtest.js
+++ app/assets/javascripts/gridtest.js
@@ -1,63 +1,82 @@
 
-var gridSizePx = 50
-var gridBorderPx = 1
-var gridWidthHeight = gridSizePx - (2 * gridBorderPx)
-var mapWidth = $( "#map" ).width()
-var mapHeight = $( "#map" ).height()
+$( "select#size" ).on("change",function(){
 
-var numWide = parseInt(mapWidth/gridSizePx)
-var numHigh = parseInt(mapHeight/gridSizePx)
+  // Remove all existing grid square if there are any
+  $( ".gridsq" ).remove();
 
-var numSquares = numWide * numHigh
+  // Add first grid square
+  $( "#map" ).append( '<div class="gridsq" id="sq0" tabindex="1"></div>' );
 
-//console.log("numSquares:" + numSquares)
+  var gridSizeSelected = $( "select#size" ).val()
+  var gridSizePx = parseInt(gridSizeSelected, 10)
+  //var gridSizePx = 50
 
-$(".gridsq").css( "width", gridWidthHeight )
-$(".gridsq").css( "height", gridWidthHeight )
-$(".gridsq").css( "border-width", gridBorderPx+"px" )
+  //console.log("gridSizePx:" + gridSizePx)
 
-for (var i = 1; i < numSquares; i++) {
-  $("#sq0").clone().attr('id', 'sq'+ i).appendTo("#map")
-}
+  var gridBorderPx = 1
+  var gridWidthHeight = gridSizePx - (2 * gridBorderPx)
+  var mapWidth = $( "#map" ).width()
+  var mapHeight = $( "#map" ).height()
 
-$(".gridsq").click(function () {
-        $(this).toggleClass('on')
-});
+  var numWide = parseInt(mapWidth/gridSizePx, 10)
+  var numHigh = parseInt(mapHeight/gridSizePx, 10)
 
-$(".gridsq").on('keydown',function(e) {
-    if (e.key === ' ' || e.key === 'Spacebar')  {
-        e.preventDefault()
-        $(this).click()
-    }
-    if (e.keyCode == '38') {
-        // up arrow
-        e.preventDefault()
-        var thisSqId = $(this).attr('id')
-        $("#sq"+moveSq(thisSqId,-16)).focus()
-    }
-    else if (e.keyCode == '40') {
-        // down arrow
-        e.preventDefault()
-        var thisSqId = $(this).attr('id')
-        $("#sq"+moveSq(thisSqId,16)).focus()
-    }
-    else if (e.keyCode == '37') {
-       // left arrow
-       e.preventDefault()
-       var thisSqId = $(this).attr('id')
-       $("#sq"+moveSq(thisSqId,-1)).focus()
-    }
-    else if (e.keyCode == '39') {
-       // right arrow
-       e.preventDefault()
-       var thisSqId = $(this).attr('id')
-       $("#sq"+moveSq(thisSqId,1)).focus()
-    }
+  //console.log("numWide:" + numWide)
 
-})
+  var numSquares = numWide * numHigh
+
+  //console.log("numSquares:" + numSquares)
+
+  $( ".gridsq" ).css( "width", gridWidthHeight )
+  $( ".gridsq" ).css( "height", gridWidthHeight )
+  $( ".gridsq" ).css( "border-width", gridBorderPx+"px" )
+
+  for (var i = 1; i < numSquares; i++) {
+    $( "#sq0" ).clone().attr('id', 'sq'+ i).appendTo( "#map" )
+  }
+
+
+// Select square functions
+    $( ".gridsq" ).click(function () {
+            $( this ).toggleClass('on')
+    });
+
+    $( ".gridsq" ).on('keydown',function(e) {
+        if (e.key === ' ' || e.key === 'Spacebar')  {
+            e.preventDefault()
+            $( this ).click()
+        }
+        if (e.keyCode == '38') {
+            // up arrow
+            e.preventDefault()
+            var thisSqId = $(this).attr('id')
+            $( "#sq"+moveSq(thisSqId, (numWide*-1) ) ).focus()
+        }
+        else if (e.keyCode == '40') {
+            // down arrow
+            e.preventDefault()
+            var thisSqId = $(this).attr('id')
+            $( "#sq"+moveSq(thisSqId,numWide) ).focus()
+        }
+        else if (e.keyCode == '37') {
+           // left arrow
+           e.preventDefault()
+           var thisSqId = $(this).attr('id')
+           $( "#sq"+moveSq(thisSqId,-1) ).focus()
+        }
+        else if (e.keyCode == '39') {
+           // right arrow
+           e.preventDefault()
+           var thisSqId = $(this).attr('id')
+           $( "#sq"+moveSq(thisSqId,1) ).focus()
+        }
+
+    })
+
+}).trigger("change") // extra call to initialise at start  // end onchange
 
 function moveSq(thisSqId,dist){
-  var thisSqNum = parseInt(thisSqId.replace("sq", "", 10))
+  var thisSqNum = parseInt(thisSqId.replace("sq", ""), 10)
   var nextSqNum = thisSqNum + dist
   var nextSqNumStr = nextSqNum.toString()
   return nextSqNumStr
