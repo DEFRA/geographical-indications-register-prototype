93f54f7b4c37665ad9a945a9eddfb515d0be35f6
Martin Glancy <glancy@gmail.com>
2020-09-15 15:23:59 +0100

    Add v4 files

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ router.use('/v0', require('./routes_v0'))
 router.use('/v1', require('./routes_v1'))
 router.use('/v2', require('./routes_v2'))
 router.use('/v3', require('./routes_v3'))
+router.use('/v4', require('./routes_v4'))
 router.use('/fcrm-1', require('./fcrm-1'))
 
 module.exports = router
